9cd9ecd84884bb0852476f6e34a6743210d9c9c7
pwill0217 <73377167+pwill0217@users.noreply.github.com>
2024-02-23 00:16:20 -0500

    Create inputage.py

---
+++ inputage.py
@@ -0,0 +1,7 @@
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+age = int(input('How old are you?:'))
+year= 2024
+print('You will be 100 years old in', year - age + 100)
+print(age)
